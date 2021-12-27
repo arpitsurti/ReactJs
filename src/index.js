e3a522f564f3ccb731b7e376a8177951b3f24df7
Arpit S <arpittempacc@gmail.com>
2021-12-27 18:56:10 -0500

    Introduction to element and components

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //Without JSX
-const element = React.createElement("<h1>",null,"My first react without jsx");
-ReactDOM.render(element,document.getElementById("root"));
+// const element = React.createElement("h1",null,"My first react without jsx");
+// ReactDOM.render(element,document.getElementById("root"));
 
 //Using JSX
+// const element = <h1>My first React using jsx</h1>
 // ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+//     element
 //   ,
 //   document.getElementById('root')
 // );
@@ -18,3 +17,35 @@ ReactDOM.render(element,document.getElementById("root"));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
+
+var DisplayEmployees =(emp)=> {
+    return <div>
+        <p>
+            <label><b>Id: </b> {emp.id}</label>
+        </p>
+        <p>
+            <label><b>First Name: </b> {emp.fName}</label>
+            </p>
+        <p>
+            <label><b>Last Name: </b> {emp.lName}</label>
+        </p>
+        <p>
+            <label><b>Location: </b> {emp.location}</label>
+        </p>
+        <DisplayEmployeeDept deptName={emp.deptName} headName={emp.headName}></DisplayEmployeeDept>
+    </div>;
+}
+
+var DisplayEmployeeDept =(dept)=> {
+    return <div>
+        <p>
+            <label><b>Dept Name: </b> {dept.deptName}</label>
+            </p>
+        <p>
+            <label><b>Head Name: </b> {dept.headName}</label>
+        </p>
+    </div>;
+}
+const element = <DisplayEmployees id="1" fName="John" lName="Doe" location="USA"
+DisplayEmployeeDept deptName="I.T" headName="head1"></DisplayEmployees>
+ReactDOM.render(element,document.getElementById("root"));

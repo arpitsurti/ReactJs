8755d4e5508d5c8ecb12e6df8987d5b6e93689fc
Arpit S <arpittempacc@gmail.com>
2021-12-28 15:30:07 -0500

    7. Interaction between two components

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import './index.css';
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
-
+//4. 
 // var DisplayEmployees =(emp)=> {
 //     return <div>
 //         <p>
@@ -47,7 +47,7 @@ import './index.css';
 //     </div>;
 // }
 
-//Using class component
+//5. Using class component
 // class Employee extends React.Component{
 //     constructor (props)
 //     {
@@ -91,24 +91,112 @@ import './index.css';
 // deptName="I.T" headName="head1"></Employee>
 // ReactDOM.render(element,document.getElementById("root"));
 
-//USING STATE IN REACT
+//6. USING STATE IN REACT
+// class Employee extends React.Component
+// {
+//     state = {
+//         counter:0
+//     };
+//     addEmployee=()=>{
+//         this.setState({counter:this.state.counter+1});
+//     }
+
+//     render()
+//     {
+//         return <div>
+//             <h1>Welcome to the employee component</h1>
+//             <p><button onClick={this.addEmployee}>Increment employee count</button></p>
+//             <p><label>Total count {this.state.counter}</label></p>
+//         </div>
+//     }
+// }
+// const element = <Employee></Employee>
+// ReactDOM.render(element,document.getElementById("root"));
+
+//7. INTERACTION BETWEEN THE COMPONENTS
 class Employee extends React.Component
 {
-    state = {
-        counter:0
-    };
-    addEmployee=()=>{
-        this.setState({counter:this.state.counter+1});
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            updatedSalary:null
+        };
+    }
+
+    getUpdatedSalary=(salary)=>
+    {
+        this.setState({updatedSalary : salary});
     }
 
     render()
     {
         return <div>
-            <h1>Welcome to the employee component</h1>
-            <p><button onClick={this.addEmployee}>Increment employee count</button></p>
-            <p><label>Total count {this.state.counter}</label></p>
-        </div>
+            <h1>Employee Component</h1>
+            <p>
+              <label><b>Id: </b> {this.props.id}</label>
+            </p>
+            <p>
+              <label><b>First Name: </b> {this.props.fName}</label>
+            </p>
+            <p>
+              <label><b>Last Name: </b> {this.props.lName}</label>
+            </p>
+            <p>
+              <label><b>Location: </b> {this.props.location}</label>
+            </p> 
+            <p>
+              <label><b>Salary: </b> {this.props.Salary}</label>
+            </p> 
+            <p>
+              <label><b>Updated Salary: </b> {this.state.updatedSalary}</label>
+            </p>
+            <Salary HRA={this.props.HRA} basicSalary={this.props.Salary}
+            SAllowance={this.props.SAllowance} onSalaryChanged={this.getUpdatedSalary}></Salary>
+        </div>;
     }
 }
-const element = <Employee></Employee>
-ReactDOM.render(element,document.getElementById("root"));
+
+class Salary extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.basicRef =React.createRef();
+        this.hraRef =React.createRef();
+        this.saRef =React.createRef();
+        this.state = {
+            bs : this.props.basicSalary,
+            hr : this.props.HRA,
+            sa : this.props.SAllowance,
+        }
+    }
+
+    UpdateSalary=()=>{
+        let salary = parseInt(this.basicRef.current.value)+ parseInt(this.hraRef.current.value)
+        + parseInt(this.saRef.current.value);
+        this.props.onSalaryChanged(salary);
+    }
+
+    render()
+    {
+        return <div>
+            <h1>Salary Component</h1>
+            <p>
+              <label><b>Basic Salary: </b> <input type="text" defaultValue={this.state.bs} ref={this.basicRef}></input></label>
+            </p>
+            <p>
+              <label><b>HRA: </b><input type="text" ref={this.hraRef}  defaultValue={this.state.hr}></input></label>
+            </p>
+            <p>
+              <label><b>Special Allowance: </b><input type="text" ref={this.saRef}  defaultValue={this.state.sa}></input></label>
+            </p> 
+            <button onClick={this.UpdateSalary}>Update</button>
+        </div>;
+    }
+}
+const element = <Employee id="1" fName="John" lName="Doe" location="USA"
+ Salary="50000" basicSalary="30000" HRA="15000" SAllowance="5000"
+ ></Employee>
+
+ ReactDOM.render(element,document.getElementById("root"));
